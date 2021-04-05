f3c2490259b6adb334938973af6dd1a67a171564
Conqu3red <Conqu3red@users.noreply.github.com>
2021-04-05 07:54:07 +0100

    better compile script

--- win-src/compile.bat
+++ win-src/compile.bat
@@ -1,3 +1,7 @@
-pyinstaller --onefile --icon=icon.ico ecp.py
+@echo off
+copy /Y ..\src\ecp.py .
+copy /Y ..\src\lexer.py .
+copy /Y ..\src\parse.py .
+pyinstaller --onefile --icon=icon.ico --exclude tkinter ecp.py
 pyinstaller --onefile --windowed --icon=icon.ico ECPUser.py
 pause
