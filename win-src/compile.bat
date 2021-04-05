dfbeea30b1b69b2179252fa8cceca5dea1c56223
Conqu3red <Conqu3red@users.noreply.github.com>
2021-04-05 14:27:05 +0100

    fix compile script

--- win-src/compile.bat
+++ win-src/compile.bat
@@ -2,8 +2,14 @@
 copy /Y ..\src\ecp.py .
 copy /Y ..\src\lexer.py .
 copy /Y ..\src\parse.py .
-pyinstaller --onefile --icon=icon.ico --exclude tkinter ecp.py
+pyinstaller ^
+ --onefile ^
+ --icon=icon.ico ^
+ --exclude tkinter ^
+ --exclude multiprocessing ^
+ --exclude unittest ^
+ --exclude urllib ^
+ ecp.py
 pyinstaller --onefile --windowed --icon=icon.ico ECPUser.py
 copy /Y dist\* ..\executables
-iscc ..\executables\compileiss.iss
 pause
