33af4a12d79bc5005fe87a6c7d99072a053a496d
Conqu3red <Conqu3red@users.noreply.github.com>
2021-04-18 16:03:38 +0100

    fix compile script

--- src/ecp.py
+++ src/ecp.py
@@ -8,7 +8,7 @@ from traceback import print_exc
 parser = argparse.ArgumentParser(description="ECP interpreter")
 parser.add_argument("inputfile", type=argparse.FileType("r", encoding="utf-8"), nargs="?")
 parser.add_argument("--debug", action="store_true", help="show debug information like token list")
-parser.add_argument("--trace", action="store", nargs="*", help="space seperated names of the variables to be traced")
+parser.add_argument("--trace", action="store", nargs="*", default=[], help="space seperated names of the variables to be traced")
 parser.add_argument("--tracecompact", action="store_true", help="trace compactly")
 parser.add_argument("--pause", action="store_true", help="pause on completion")
 parser.add_argument('--version', action='version', version='%(prog)s v'+__version__)
--- win-src/compile.bat
+++ win-src/compile.bat
@@ -1,5 +1,6 @@
 @echo off
 copy /Y ..\src\ecp.py .
+copy /Y ..\src\tracker.py .
 copy /Y ..\src\lexer.py .
 copy /Y ..\src\parse.py .
 del /Q dist\ecp\*
