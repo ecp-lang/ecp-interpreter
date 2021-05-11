b421c4725f0b95635cae6db0bc40f190dff84298
Conqu3red <Conqu3red@users.noreply.github.com>
2021-05-11 19:23:58 +0100

    pip module!

--- src/ecp/__init__.py
+++ src/ecp/__init__.py
@@ -1,3 +1,4 @@
+__version__ = "1.2.0-b1"
 from .lexer import Token, TokenType, Lexer, LexerResult
 from .parse import Object, IntObject, FloatObject, BoolObject, ArrayObject, StringObject, DictionaryObject, BuiltinModule
 from .parse import __interpreter__
