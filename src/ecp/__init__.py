211bd6935302d08cca76ba4f650ac47ef70b48f3
Conqu3red <46159994+Conqu3red@users.noreply.github.com>
2021-09-17 21:10:42 +0100

    version bump

--- src/ecp/__init__.py
+++ src/ecp/__init__.py
@@ -3,6 +3,6 @@
 An implementation of the ECP programming language. Includes a Lexer and Parser for generating a python AST from
 ECP code.
 """
-__version__ = "1.4.1"
+__version__ = "1.4.2"
 from .lexer import EcpLexer
 from .topython import EcpParser, ecp, parse_ecp, to_py_source, _dump, get_more
